dc3122ecb4e7ddcba6fceed97cd0ff4f03cb4795
Thibault <dpzthibault@gmail.com>
2020-08-06 19:30:53 +0200

    Initial project setup

---
+++ AlertModaleManager/AlertModaleManager.h
@@ -0,0 +1,19 @@
+//
+//  AlertModaleManager.h
+//  AlertModaleManager
+//
+//  Created by Thibault on 06/08/2020.
+//  Copyright © 2020 Thibault. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlertModaleManager.
+FOUNDATION_EXPORT double AlertModaleManagerVersionNumber;
+
+//! Project version string for AlertModaleManager.
+FOUNDATION_EXPORT const unsigned char AlertModaleManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertModaleManager/PublicHeader.h>
+
+
---
+++ AlertModaleManagerExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertModaleManagerExamples
+//
+//  Created by Thibault on 06/08/2020.
+//  Copyright © 2020 Thibault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
